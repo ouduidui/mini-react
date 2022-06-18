2bd8076300a69529e1750fe1c2501fdc2f03c1c0
Dewey Ou <oushihao97@gmail.com>
2022-06-18 17:59:38 +0800

    feat: react dom

---
+++ packages/react-dom/ReactDOM.ts
@@ -0,0 +1,11 @@
+import type { ReactNodeList } from 'shared/ReactType'
+
+interface RootType {
+  render: (children: ReactNodeList) => void
+}
+
+export function createRoot(
+  container: Element,
+) {
+
+}
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,10 +5,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   resolve: {
     alias: {
-      'react/': `${path.resolve(__dirname, 'packages/react/src')}/`,
-      'react-dom/': `${path.resolve(__dirname, 'packages/react-dom/src')}/`,
-      'shared/': `${path.resolve(__dirname, 'packages/react/src')}/`,
-      'react-reconciler/': `${path.resolve(__dirname, 'packages/react-reconciler/src')}/`,
+      'react/': `${path.resolve(__dirname, 'packages/react')}/`,
+      'react-dom/': `${path.resolve(__dirname, 'packages/react-dom')}/`,
+      'shared/': `${path.resolve(__dirname, 'packages/react')}/`,
+      'react-reconciler/': `${path.resolve(__dirname, 'packages/react-reconciler')}/`,
     },
   },
 })
